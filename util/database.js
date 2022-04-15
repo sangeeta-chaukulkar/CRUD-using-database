62e957cb50224687987c111e5e45343fb9aa9797
sangeeta-chaukulkar <sangeetachaukulkar05@gmail.com>
2022-04-15 12:37:07 +0530

    crud using sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,4 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'nodecomplete',
-    password: 'root'
-});
-
-module.exports = pool.promise();
+const sequelize=new Sequelize('nodecomplete','root','root',{dialect:'mysql',host:'localhost'});
+module.exports = sequelize;
