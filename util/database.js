94e0757d2c423015706b740b980b5b452ecf0af4
sangeeta-chaukulkar <sangeetachaukulkar05@gmail.com>
2022-04-15 11:46:59 +0530

    commit

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'nodecomplete',
-    password: 'Yash123$'
+    password: 'root'
 });
 
 module.exports = pool.promise();
